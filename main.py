24282f583038c342d7f8138652c5060b09dff218
Jooleric <bernardjoseph7872gmail.com>
2025-08-10 18:17:54 +0100

    Initial commit

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# main.py
+
+from book_class import Book
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+
+    print(my_book)
+    print(repr(my_book))
+    del my_book
+if __name__ == "__main__":
+    main()
