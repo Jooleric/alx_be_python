7871d6c5153c246b6546418c288bbd9bb278b5cd
Jooleric <bernardjoseph7872gmail.com>
2025-08-12 10:11:26 +0100

    Restore

--- fns_and_dsa/python_introduction/hours_to_seconds.py
+++
@@ -1,8 +0,0 @@
-# Define the number of hours
-hours = 2
-
-# Convert hours to seconds
-seconds = hours * 60 * 60
-
-# Print the result
-print(hours, "hour(s) is", seconds, "seconds.")
--- fns_and_dsa/python_introduction/rectangle_area.py
+++
@@ -1,9 +0,0 @@
-# Define length and width
-length = 10
-width = 5
-
-# Calculate area
-area = length * width
-
-# Print the result
-print("The area of the rectangle is:", area)
